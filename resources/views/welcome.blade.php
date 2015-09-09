b0c107851650e87421bf5ac0b5be18617dfe6eb2
Manix <vigneshmanix@gmail.com>
2015-09-09 13:12:58 +0530

    home page rearrangements

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,70 +20,75 @@
     <li><a href="/sponsorship_opportunity">Sponsorship Opportunity </a></li>
 @stop
 @section('content')
-<div class="row">
-    <div class="col s12">
-        <div class="card white" style="padding-top:0px;">
-            <div class="card-content black-text ">
-                <div class="row valign-wrapper">
-                    <div class="col s12 m12 l3 valign">
-                        <center><img  src="/images/nitlogo.png" height="170px"></center>
+    <div class="row">
+        <div class="col s12">
+            <div class="card white" style="padding-top:0px;">
+                <div class="card-content black-text ">
+                    <div class="row valign-wrapper">
+                        <div class="col s12 m12 l3 valign">
+                            <center><img  src="/images/nitlogo.png" height="120px"></center>
+                        </div>
+                        <div class="col s12 m12 l6 valign">
+                            <center>
+                                <h6 style="font-size: 20px;">Employability</h6>
+                                <h5 style="word-spacing: 15px;">SKILL ENHANCEMENT PROGRAMME</h5>
+                                <h6 style="font-size: 20px;">For College Students</h6>
+                            </center>
+                        </div>
+                        <div class="col s12 m12 l3 valign">
+                            <center><img src="/images/doms.png"  height="100px"></center>
+                        </div>
                     </div>
-                    <div class="col s12 m12 l6 valign">
-                        <center>
-                            <h5>SKILL ENHANCEMENT PROGRAMME</h5>
-                        </center>
-                    </div>
-                    <div class="col s12 m12 l3 valign">
-                        <center><img class="responsive-img" src="/images/doms.png"  height="170px"></center>
-                    </div>
-                </div>
-                <div class="col s6 offset-s3">
-                    <center><h6 style="font-size:21px;"><strong>9<sup>th</sup> October</strong> (Friday) and <strong>10<sup>th</sup> October</strong> (Saturday)</h6></center>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<div class="row">
-    <div class="col s12 m12 l6">
-        <div class="card " style="min-height:290px;">
-            <div class="card-content black-text">
-                <br>
-                <h5>About the Programme</h5>
-                <hr>
-                <p style="text-align:justify" >
-                Department of Management Studies, NIT Trichy is organizing a Two-day employability Skill Enhancement Programme (SEP) for college students on 9th and 10th October, 2015. The training programme aims to enlighten the students in realizing their potential and building confidence to face the job/employment.
-                </p>
-            </div>
-        </div>
-    </div>
-    
-    <div class="col s12 m12 l4">
-        <div style="min-height:290px" class="card white">
-            <div class="card-content black-text">
-                <h5>Programme Co-ordinator :</h5>
-                <br>
-                <div>
-                <h6>Dr.G.MURUGANANTHAM</h6>
-                <p>MBA Dept, NIT -Trichy, 620015.</p>
-                <hr>
-                <p>Mobile&nbsp;:&nbsp;9865175135</p>
-                <p>Email &nbsp;:&nbsp;marcon@nitt.edu</p>
+    <div class="row">
+        <div class="col s12">
+            <div class="card">    
+                <div class="card-content">
+                    <center>
+                        <h6 style="font-size:21px;">
+                            <strong>9<sup>th</sup> October</strong> (Friday) and <strong>10<sup>th</sup> October</strong> (Saturday)
+                        </h6>
+                    </center>
                 </div>
             </div>
         </div>
+    </div>
+    <div class="row">
+        <div class="col s10">
+            <div class="card " style="min-height:290px;">
+                <div class="card-content black-text">
+                    <br>
+                    <h5>About the Programme</h5>
+                    <hr>
+                    <p style="text-align:justify" >
+                        Department of Management Studies, NIT Trichy is organizing a Two-day employability Skill Enhancement Programme (SEP) for college students on 9th and 10th October, 2015. The training programme aims to enlighten the students in realizing their potential and in building confidence to face the employment opportunities.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <img class="col s2" style="margin-top: 0.5rem;
+                                           margin-right: 0px;
+                                           margin-bottom: 1rem;
+                                           margin-left: 0px;" src="/images/lamp.gif" height="290px">    
     </div>  
+    <div class="row">
+        <div class="col s5 offset-s8">
+            <div style="min-height:200px" class="card white">
+                <div class="card-content black-text">
+                    <h5>Programme Co-ordinator</h5>
+                    <div>
+                        <h6>Dr.G.MURUGANANTHAM</h6>
+                        <p>MBA Dept, NIT -Trichy, 620015.</p>
+                        <hr>
+                        <p>Mobile : 9865175135 , Email : marcon@nitt.edu</p>
+                    </div>
+                </div>
+            </div>
+        </div>  
 
-        
-        
-        
-                <img class="col s12 m12 l2" style="margin-top: 0.5rem;
-margin-right: 0px;
-margin-bottom: 1rem;
-margin-left: 0px;" src="/images/lamp.gif" height="290px">    
-        
-        
-        
-    </div>  
-</div>
+    </div>
+
 @stop
